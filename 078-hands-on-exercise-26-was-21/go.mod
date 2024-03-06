b408ef1e811362af786435a55e54395e71537c68
0xab42 <0xab42@gmail.com>
2024-03-06 09:42:40 +0700

    solution for 26 old 21

---
+++ 078-hands-on-exercise-26-was-21/go.mod
@@ -0,0 +1,3 @@
+module 078-hands-on-exercise-26-was-21
+
+go 1.22.0
---
+++ 078-hands-on-exercise-26-was-21/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("I am init func")
+}
+
+func main() {
+	fmt.Println("I am main func")
+}
