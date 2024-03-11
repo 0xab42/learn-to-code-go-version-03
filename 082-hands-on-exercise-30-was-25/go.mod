9dd57df5d37d82b0ca9d1f47c512c7e016d0673c
0xab42 <0xab42@gmail.com>
2024-03-12 06:49:33 +0700

    solution for 30 old 25

---
+++ 082-hands-on-exercise-30-was-25/go.mod
@@ -0,0 +1,3 @@
+module 082-hands-on-exercise-30-was-25
+
+go 1.22.0
---
+++ 082-hands-on-exercise-30-was-25/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	for y := 1; y < 43; y++ {
+		fmt.Printf("y: %v,\t", y)
+		x := rand.Intn(5)
+
+		switch x {
+		case 0:
+			fmt.Println("x = 0")
+		case 1:
+			fmt.Println("x = 1")
+		case 2:
+			fmt.Println("x = 2")
+		case 3:
+			fmt.Println("x = 3")
+		case 4:
+			fmt.Println("x = 4")
+		default:
+			fmt.Println("x = ", x)
+		}
+	}
+}
